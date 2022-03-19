4adc0879ad7d2c5149fa01877b95bc4271a4b3ab
Philip M. Gollucci <pgollucci@p6m7g8.com>
2022-03-18 21:03:39 -0400

    chore: relocates to p6m7g8-dotfiles (#1)

--- init.zsh
+++ init.zsh
@@ -8,7 +8,7 @@
 ######################################################################
 p6df::modules::slack::deps() {
   ModuleDeps=(
-    p6m7g8/p6common
+    p6m7g8-dotfiles/p6common
     rockymadden/slack-cli
   )
 }
