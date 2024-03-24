82560d7081862307f461612f7d0bf57ab5d35b1a
Philip M. Gollucci <pgollucci@p6m7g8.com>
2024-03-24 13:44:37 -0400

    chore: libify (#2)

--- init.zsh
+++ init.zsh
@@ -16,98 +16,22 @@ p6df::modules::slack::deps() {
 ######################################################################
 #<
 #
-# Function: p6df::modules::slack::init()
+# Function: p6df::modules::slack::init(_module, dir)
+#
+#  Args:
+#	_module -
+#	dir -
 #
 #  Environment:	 P6_DFZ_SRC_DIR
 #>
 ######################################################################
 p6df::modules::slack::init() {
+  local _module="$1"
+  local dir="$2"
+
+  p6_bootstrap "$dir"
 
   p6_path_if "$P6_DFZ_SRC_DIR/rockymadden/slack-cli/src"
 
   p6_return_void
-}
-
-######################################################################
-#<
-#
-# Function: p6df::modules::slack::chatdelete(channel, timestamp, token)
-#
-#  Args:
-#	channel -
-#	timestamp -
-#	token -
-#
-#  Environment:	 POST SLACK_CLI_TOKEN
-#>
-######################################################################
-p6df::modules::slack::chatdelete() {
-  local channel=$1
-  local timestamp=$2
-  local token=$SLACK_CLI_TOKEN
-
-  local msg=$(curl -s -X POST https://slack.com/api/chat.delete \
-    --data-urlencode "as_user=true" \
-    --data-urlencode "channel=${channel}" \
-    --data-urlencode "ts=${timestamp}" \
-    --data-urlencode "token=${token}")
-
-  echo $msg | jq .
-}
-
-######################################################################
-#<
-#
-# Function: p6df::modules::slack::chatsend(channel, text, token)
-#
-#  Args:
-#	channel -
-#	text -
-#	token -
-#
-#  Environment:	 POST SLACK_CLI_TOKEN
-#>
-######################################################################
-p6df::modules::slack::chatsend() {
-  local channel=$1
-  local text=$2
-  local token=$SLACK_CLI_TOKEN
-
-  local msg=$(curl -s -X POST https://slack.com/api/chat.postMessage \
-    --data-urlencode "as_user=true" \
-    --data-urlencode "channel=${channel}" \
-    --data-urlencode "text=${text}" \
-    --data-urlencode "token=${token}")
-
-  echo $msg | jq .
-}
-
-######################################################################
-#<
-#
-# Function: p6df::modules::slack::chatupdate(channel, timestamp, text, token)
-#
-#  Args:
-#	channel -
-#	timestamp -
-#	text -
-#	token -
-#
-#  Environment:	 POST SLACK_CLI_TOKEN
-#>
-######################################################################
-p6df::modules::slack::chatupdate() {
-  local channel=$1
-  local timestamp=$2
-  local text=$3
-  local token=$SLACK_CLI_TOKEN
-
-  local msg=$(curl -s -X POST https://slack.com/api/chat.update \
-    --data-urlencode "as_user=true" \
-    --data-urlencode "channel=${channel}" \
-    --data-urlencode "ts=${timestamp}" \
-    --data-urlencode "text=${text}" \
-    --data-urlencode "token=${token}")
-
-  echo $msg | jq .
 }
